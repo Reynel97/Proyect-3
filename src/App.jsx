670f996f12fc285c81fdca821a1643592c29bb36
Reinaldo Jose <reynaldojosedelacruzrojano@gmail.com>
2023-07-07 01:02:49 -0500

    Estado-activo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
+import GetRamdonNumber from './utils/getRamdonNumber'
 
 function App() {
   
+  const [location , setLocation] = useState()
+
+  useEffect(() => {
+    axios
+    .get(`https://rickandmortyapi.com/api/location/${GetRamdonNumber(126)}`)
+    .then(res => setLocation(res.data))
+    .catch(error => console.log(error))
+  },[])
+
+  console.log(location)
 
   return (
     <>
-     
+     <div>
+      <h1>Rick And Morty App</h1>
+     </div>
     </>
   )
 }
---
+++ src/utils/GetRamdonNumber.js
@@ -0,0 +1,6 @@
+const GetRamdonNumber = () => {
+
+    return Math.floor(Math.random() *126 ) + 1
+}
+
+export default GetRamdonNumber
